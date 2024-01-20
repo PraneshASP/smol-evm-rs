c8b0243359bfbd7eca12b5108919bda86d3ea30c
PraneshASP <praneshas2000@gmail.com>
2024-01-21 00:24:08 +0530

    chore: 🔨 add step count

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,22 @@ use smol_evm_rs::{
 use std::env::args;
 
 fn main() {
+    let max_steps = 1000; // Todo: Should replace with gas
     Opcodes::register_instructions();
 
     let hex_str = args().nth(1).expect("No bytecode passed");
     let byte_array = hex::decode(&hex_str).expect("Invalid hex string");
     let code = Bytes::from(byte_array);
     let mut context = ExecutionContext::new(code);
-
+    let mut steps = 1;
     while !context.stopped {
         let pc_before = context.pc.clone();
         let instruction = Instruction::decode_opcode(&mut context).unwrap();
         instruction.executor.execute(&mut context);
-
+        steps += 1;
+        if steps > max_steps {
+            panic!("Error: Out of gas"); // Todo: Handle gracefully
+        }
         println!("{:?} @ pc={}", instruction.name, pc_before);
         println!("Stack: {:?}", context.stack.stack);
         println!("Memory: {:?}", context.memory.memory);
